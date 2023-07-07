bb0f8c4820aaf183793d7279eeaad43a34044311
Omed <Omedakram54@gmail.com>
2023-07-07 23:06:26 +0300

    tsConfig, interface, type, union, enum

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,14 @@ console.log(b);
 const select = b.add({ id: 4, name: "azhi", age: 24 });
 
 function selectList<T extends keyof List>(key: T, list: List[]) {
-  return list.map((item) => item[key]);
+  return list.flatMap((item) => item[key]);
 }
 
-console.log(selectList("name", list));
+// const total = () => {
+//   return list.reduce((prev, curr) => {
+//     return prev + curr.age;
+//   }, 0);
+// };
+
+// console.log(total());
+console.log(selectList("age", list));
