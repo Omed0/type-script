9a0b935d17412c391b1bcbdef7e0cddac356dcaf
Omed <Omedakram54@gmail.com>
2023-07-08 22:43:01 +0300

    lesson three class

--- build/js/index.js
+++ build/js/index.js
@@ -1,7 +1,28 @@
-const list = [
-    { id: 1, name: "ahmad", age: 18 },
-    { id: 2, name: "ali", age: 20 },
-    { id: 3, name: "reza", age: 22 },
-];
-const select = list.find((item) => item.name === "reza" && item.age >= 18);
-console.log(select);
+"use strict";
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.list = [
+            { id: 1, name: "ahmad", age: 18 },
+            { id: 2, name: "ali", age: 20 },
+            { id: 3, name: "reza", age: 22 },
+        ];
+        this.name = name;
+        this.age = age;
+    }
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+    addCount() {
+        let count = 0;
+        setInterval(() => {
+            while (true) {
+                count++;
+                console.log(count);
+            }
+        }, 1500);
+    }
+}
+const user = new User("ahmad", 18);
+console.log(user.addCount());
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
-interface List {
+type List = {
   id: number;
   name: string;
   age: number;
+};
+
+class User {
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  public sayHi(): void {
+    console.log(`Hi, I am ${this.name}`);
+  }
+
+  list: List[] = [
+    { id: 1, name: "ahmad", age: 18 },
+    { id: 2, name: "ali", age: 20 },
+    { id: 3, name: "reza", age: 22 },
+  ];
+
+  addCount(): void {
+    let count = 0;
+    setInterval(() => {
+      while (true) {
+        count++;
+      }
+    }, 1000);
+  }
 }
 
-const list: List[] = [
-  { id: 1, name: "ahmad", age: 18 },
-  { id: 2, name: "ali", age: 20 },
-  { id: 3, name: "reza", age: 22 },
-];
-const b = new Set(list);
-console.log(b);
-
-const select = b.add({ id: 4, name: "azhi", age: 24 });
-
-function selectList<T extends keyof List>(key: T, list: List[]) {
-  return list.flatMap((item) => item[key]);
-}
-
-// const total = () => {
-//   return list.reduce((prev, curr) => {
-//     return prev + curr.age;
-//   }, 0);
-// };
-
-// console.log(total());
-console.log(selectList("age", list));
+const user = new User("ahmad", 18);
+console.log(user.addCount());
