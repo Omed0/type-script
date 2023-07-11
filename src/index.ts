55822cbefa290d1653e8295b18858130f2f83fa3
Omed <Omedakram54@gmail.com>
2023-07-11 23:45:10 +0300

    noUncheckedIndexedAccess, ts-reset

--- src/index.ts
+++ src/index.ts
@@ -101,3 +101,45 @@ const userIdentity = <T extends HasID>(user: T): T => {
 
 console.log(userIdentity({ id: 1, name: "ahmad" }));
 // console.log(userIdentity({ name: "ahmad" })); // requierd id if it is not get error
+
+const getUserProperty = <T extends HasID, K extends keyof T>(
+  users: T[],
+  key: K
+): T[K][] => {
+  return users.map((user) => user[key]);
+};
+
+const users = [
+  {
+    id: 1,
+    name: "ahmad akram",
+    username: "ahmad",
+    email: "ahmad@gmail.com",
+    address: {
+      city: {
+        city: "Gwenborough",
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        zipcode: "92998-3874",
+      },
+    },
+  },
+  {
+    id: 2,
+    name: "ali fatah",
+    username: "ali",
+    email: "ali@gmail.com",
+    address: {
+      city: {
+        city: "Gwenborough",
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        zipcode: "92998-3874",
+      },
+    },
+  },
+];
+
+console.log(getUserProperty(users, "name"));
+console.log(getUserProperty(users, "email"));
+console.log(getUserProperty(users, "address"));
