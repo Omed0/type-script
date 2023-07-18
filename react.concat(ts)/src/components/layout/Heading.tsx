606418e74e9cf2054521a1b8ef4dba2b1b035a9e
Omed <Omedakram54@gmail.com>
2023-07-18 23:58:49 +0300

    practice type script with api

--- react.concat(ts)/src/components/layout/Heading.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-type HeadingProps = {
-  title: string;
-};
-
-export default function heading({ title }: HeadingProps): ReactElement {
-  return <h1 className="text-4xl bold gold mt-8 text-center">{title}</h1>;
-}
